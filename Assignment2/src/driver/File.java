5fdebd18b462fc4bee256cea86cdb87bf1048c20
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-06-17 18:28:30 +0000

    Added more methods to File

--- Assignment2/src/driver/File.java
+++ Assignment2/src/driver/File.java
@@ -9,22 +9,44 @@ public class File {
   public File(String name) {
     this.name = name;
     this.contents = "";
+    this.fullPathName = name;
+    this.parentDirectory = null;
   }
   
   public File(String name, String contents) {
     this.name = name;
     this.contents = contents;
+    this.fullPathName = name;
+    this.parentDirectory = null;
   }
   
-  // TODO:
   // Method to get/set name
+  public void setName(String name) {
+    this.name = name;
+  }
   
   public String getName() {
-    
+    return name;
   }
   
   // Method to get/set contents
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
+  
+  public String getContents() {
+    return contents;
+  }
   // Method to get/set parentDirectory
+  public void setParentDirectory(Directory parentDirectory) {
+    this.parentDirectory = parentDirectory;
+    // Update the file's full path name using its parent directory
+    this.fullPathName = parentDirectory.getFullPathName() + name;
+  }
+  
+  public Directory getParentDirectory() {
+    return parentDirectory;
+  }
   
   // Do I need functions for set/get name, set/get/append contents? Yes
   // What about distinction between set and append contents? Or should this distinction be in EchoToFile?
