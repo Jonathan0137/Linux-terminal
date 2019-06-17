4687a9736a901a5850dabc643bfd75375c7b4442
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-17 18:15:59 +0000

    Added functionality to Directory, File, and FileSystem classes

--- Assignment2/src/driver/FileSystem.java
+++ Assignment2/src/driver/FileSystem.java
@@ -1,14 +1,71 @@
 package driver;
 
 public class FileSystem {
-  private String pathName;
-  private String type; // Indicates if current place is File or Directory? Maybe unnecessary, i.e. have a parent class for file/directory?
-  private Directory head;
+  
+  // Tracks information about the entire file system via the root Directory
+  private Directory root;
   
   public FileSystem() {
-    pathName = "/";
-    head = new Directory();
+    root = new Directory("");
   }
+  
+  // Not sure if there will ever be this case: it is unsafe since if root is changed, then we lose FileSystem
+  public FileSystem(Directory root) {
+    this.root = root;
+  }
+  
+  public Directory getDirectory(String fullPathName) {
+    Directory traversalDirectory = root;
+    String[] nameList = fullPathName.split("/");
+    
+    for (int i=0; i<nameList.length; i++) {
+      // Bug with String.split that includes an empty string if you start path name with "/"
+      if (nameList[i].equals("")) {
+        continue;
+      }
+      traversalDirectory = traversalDirectory.findDirectory(nameList[i]);
+      if (traversalDirectory == null) {
+        return null;
+      }
+    }
+    return traversalDirectory;
+  }
+  // Maybe unnecessary, since we can just use getDirectory("");
+  public Directory getRootDirectory() {
+    return root;
+  }
+  
+  // For TESTING PURPOSES ONLY
+  public static void main(String[] args) {
+    FileSystem fs = new FileSystem();
+    Directory root = fs.getRootDirectory();
+    
+    Directory subDir = new Directory("hello");
+    
+    root.addSubdirectory(subDir);
+    root = root.findDirectory("hello");
+    System.out.println(root.getFullPathName());
+    
+    Directory subSubDir = new Directory("world");
+    
+    root.addSubdirectory(subSubDir);
+    System.out.println(subSubDir.getFullPathName());
+    
+    Directory dirSearch = fs.getDirectory("/hello/world/");
+    
+    if (dirSearch != null) {
+      System.out.println(dirSearch.getName());
+    }
+    else {
+      System.out.println("Error, directory not found");
+    }
+   
+    
+  }
+  
+  // Maybe need a FileSystemNode that is a parent class to Directory and File,
+  // SO that I don't need to create class for finding each type 
+   
   
   // Do I need methods for changing directories and accessing subdirectories/files here?
   // That conflicts with responsibilities for Cd and other commands, but how else
