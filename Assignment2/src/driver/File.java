5abd071a9f0408369936d45b5b7bb10e626c06e9
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-06-17 13:46:42 +0000

    Added instance variables and constructors for File

--- Assignment2/src/driver/File.java
+++ Assignment2/src/driver/File.java
@@ -1,5 +1,16 @@
 package driver;
 
 public class File {
-
+  private String name; // Name of file
+  private String contents; // Text contents of the file
+  
+  public File(String name) {
+    this.name = name;
+    this.contents = "";
+  }
+  
+  public File(String name, String contents) {
+    this.name = name;
+    this.contents = contents;
+  }
 }
