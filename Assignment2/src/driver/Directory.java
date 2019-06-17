679c8366265220daf1967739c3816e156d120999
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-06-17 16:21:41 +0000

    Added methods for Directory and File classes

--- Assignment2/src/driver/Directory.java
+++ Assignment2/src/driver/Directory.java
@@ -9,20 +9,115 @@ public class Directory {
   private ArrayList<File> listOfFiles;
   
   public Directory() {
-    // Only used for creating a head directory
+    // Constructor used for creating a head directory
     this.name = "";
     this.parentDirectory = null; // Has no parent directory
     listOfSubdirectories = new ArrayList<Directory>();
     listOfFiles = new ArrayList<File>();
   }
   
-  
+  // Constructor for creating directories that are not the head of the File System
   public Directory(String name, Directory parentDirectory) {
     this.name = name;
     this.parentDirectory = parentDirectory;
     listOfSubdirectories = new ArrayList<Directory>();
     listOfFiles = new ArrayList<File>();
   }
+  
+  // Method to add a subdirectory to the list of subdirectories
+  public void addSubdirectory(Directory subDirectory) {
+    if (listOfSubdirectories.contains(subDirectory) ) { // Maybe use subDirectory.getName() if that's what is differentiating them
+      // Not sure if Directory class is responsible for printing this error?
+      System.out.println("Error: Directory '" + subDirectory.name + "' already exists.") // May need to change subDirectory.name to subDirectory.getName()
+      return;
+    }
+    
+    listOfSubdirectories.add(subDirectory);
+  }
+  
+//Method to remove a subdirectory to the list of subdirectories
+ public void removeSubdirectory(Directory subDirectory) {
+   if (!listOfSubdirectories.contains(subDirectory) ) {
+     // Not sure if Directory class is responsible for printing this error?
+     System.out.println("Error: Cannot remove '" + subDirectory.name + "': No such directory exists.") // May need to change subDirectory.name to subDirectory.getName()
+     return;
+   }
+   
+   listOfSubdirectories.remove(subDirectory);
+ }
+  
+  //Method to add a file to the list of files
+  public void addFile(File file) {
+    if (listOfFiles.contains(file) ) {
+      System.out.println("Error: File '" + file.name + "' already exists."); // May need to change file.name to file.getName()
+      return;
+    }
+    
+    listOfFiles.add(file);
+  }
+  
+  public void removeFile(File file) {
+    if (!listOfFiles.contains(file) ) {
+      // Not sure if Directory class is responsible for printing this error?
+      System.out.println("Error: Cannot remove '" + file.name + "': No such file exists."); // May need to change subDirectory.name to subDirectory.getName()
+      return;
+    }
+    
+    listOfFiles.remove(file);
+  }
+  
+  //TODO:
+  // Methods to remove files and directories -- DONE
+  // Methods to find and return files and directories in here
+  // Methods to get and set name and parentDirectory
+  // Methods to get listOfSubdirectories and listOfFiles
+  
+  public Directory findDirectory(String directoryName) {
+    for (int i=0; i<listOfSubdirectories.size(); i++) {
+      if (directoryName == listOfSubdirectories.get(i).getName()) {
+        return listOfSubdirectories.get(i);
+      }
+    }
+    // Do I need to print an error message saying DIRECTORY NOT FOUND?
+    return null;
+  }
+  
+  public File findFile(String fileName) {
+    for (int i=0; i<listOfFiles.size(); i++) {
+      if (directoryName == listOfFiles.get(i).getName()) {
+        return listOfSubdirectories.get(i);
+      }
+    }
+    // Do I need to print an error message saying DIRECTORY NOT FOUND?
+    return null;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setParentDirectory() {
+    
+  }
+  
+  public Directory getParentDirectory() {
+    
+  }
+  
+  public ArrayList<Directory> getListOfSubdirectories() {
+    
+  }
+  
+  public ArrayList<Files> getListOfFiles() {
+    
+  }
+  
+  
+  
   
   // Do I need functions for finding files/directories (does that conflict with responsibilities for Commands?), or is Cd just calling this method
 }
--- Assignment2/src/driver/File.java
+++ Assignment2/src/driver/File.java
@@ -3,6 +3,7 @@ package driver;
 public class File {
   private String name; // Name of file
   private String contents; // Text contents of the file
+  private Directory parentDirectory; // Is this necessary? To know which directory this file is in?
   
   public File(String name) {
     this.name = name;
@@ -14,5 +15,11 @@ public class File {
     this.contents = contents;
   }
   
-  // Do I need functions for set/get name, set/get/append contents?
+  // TODO:
+  // Method to get/set name
+  // Method to get/set contents
+  // Method to get/set parentDirectory
+  
+  // Do I need functions for set/get name, set/get/append contents? Yes
+  // What about distinction between set and append contents? Or should this distinction be in EchoToFile?
 }
