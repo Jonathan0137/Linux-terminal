ae301147e8eac57e9dfd4826c471148ff98a27b5
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-17 14:20:47 +0000

    Added constructor for FileSystem and ideas for designing File and DIrectory as comments

--- Assignment2/src/driver/Directory.java
+++ Assignment2/src/driver/Directory.java
@@ -4,12 +4,25 @@ import java.util.ArrayList;
 
 public class Directory {
   private String name;
-  private ArrayList<Directory> listOfSubdirectories; // Use ArrayList
+  private Directory parentDirectory; // This is necessary to access parent directory using commands such as Cd, right?
+  private ArrayList<Directory> listOfSubdirectories;
   private ArrayList<File> listOfFiles;
   
-  public Directory(String name) {
-    this.name = name;
+  public Directory() {
+    // Only used for creating a head directory
+    this.name = "";
+    this.parentDirectory = null; // Has no parent directory
     listOfSubdirectories = new ArrayList<Directory>();
     listOfFiles = new ArrayList<File>();
   }
+  
+  
+  public Directory(String name, Directory parentDirectory) {
+    this.name = name;
+    this.parentDirectory = parentDirectory;
+    listOfSubdirectories = new ArrayList<Directory>();
+    listOfFiles = new ArrayList<File>();
+  }
+  
+  // Do I need functions for finding files/directories (does that conflict with responsibilities for Commands?), or is Cd just calling this method
 }
--- Assignment2/src/driver/FileSystem.java
+++ Assignment2/src/driver/FileSystem.java
@@ -1,4 +1,16 @@
 package driver;
 
 public class FileSystem {
+  private String pathName;
+  private String type; // Indicates if current place is File or Directory? Maybe unnecessary, i.e. have a parent class for file/directory?
+  private Directory head;
+  
+  public FileSystem() {
+    pathName = "/";
+    head = new Directory();
+  }
+  
+  // Do I need methods for changing directories and accessing subdirectories/files here?
+  // That conflicts with responsibilities for Cd and other commands, but how else
+  // do the other commands access the private variables?
 }
