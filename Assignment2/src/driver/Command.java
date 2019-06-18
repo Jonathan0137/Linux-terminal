c100ef53df38fb1cbd6e8a5b998ed62aaed44f50
baichon1@UTORONTO.CA <baichon1@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-18 22:17:43 +0000

    minior changes to command and verifier

--- Assignment2/src/driver/Command.java
+++ Assignment2/src/driver/Command.java
@@ -2,8 +2,9 @@ package driver;
 
 public abstract class Command 
 {
+  protected String Doc;
   //user enteres invalid commands?
   public abstract void execute(JShell shell, String path);
   
-  
+  protected abstract String getDoc();
 }
