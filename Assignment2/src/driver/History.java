4da0aa6c1686875d67ddf8805cfe72fa25a321d7
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-18 17:48:43 +0000

    Completed the overwrittence of execute() method in History class for the history command

--- Assignment2/src/driver/History.java
+++ Assignment2/src/driver/History.java
@@ -5,16 +5,25 @@ public class History extends Command {
 	public void execute(JShell shell, String input) {
 		String[] splitInput = input.split(" ");
 		if (splitInput.length == 1) {
-			for (int i=0; i<.length(); i++) {
-				System.out.println(); //To complete
+			for (int i=0; 
+					i<shell.getInputHistory().getInputList().size(); i++) {
+				System.out.print(i+1);
+				System.out.print(". ");
+				System.out.print(shell.getInputHistory().getInputList().get(i));
 			}
 		}
 		else {
-			for (int i=0; i<.length() && 
-					i<Integer.parseInt(splitInput[1]); i++) {
-				System.out.println(); //To complete
+			if (History.historyCheck(input)) {
+				for (int i=0; i<shell.getInputHistory().getInputList().size() 
+						&& i<Integer.parseInt(splitInput[1]); i++) {
+					System.out.print(i+1);
+					System.out.print(". ");
+					System.out.print(shell.getInputHistory().getInputList().get(i));
+				}
 			}
 		}
 	}
-
+	
+	
+	
 }
