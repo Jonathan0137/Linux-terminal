75a3c3270d7f976e30aebe87f79ca3195ac9a464
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-22 14:42:11 +0000

    Added getDoc() method for Cd, and fixed bugs in the execute() method

--- Assignment2/src/driver/Cd.java
+++ Assignment2/src/driver/Cd.java
@@ -1,9 +1,22 @@
 package driver;
 
 public class Cd extends Command {
-  // Required Parent Class: ManipulationCommand
+  // TODO: Add Javadocs
+
+  public String getDoc() {
+    String documentation = "cd :cd DIR\n"
+                         + "\tChange the shell's current directory to DIR.\n\n"
+                         + "\tIf DIR begins with a slash (/), then it is interpreted\n"
+                         + "\tas an absolute path, starting from the root directory.\n"
+                         + "\tOtherwise, it is interpreted as a relative path to the\n"
+                         + "\tcurrent directory.\n\n"
+                         + "\tThe root of the file system is a single slash (/).\n\n"
+                         + "\t'..' represents the parent directory.\n"
+                         + "\t'.' represents the current directory.\n";
+    return documentation;
+  }
   
-  public Boolean execute(String input, Directory workingDir, FileSystem fs) {
+  public void execute(JShell shell, String input) {
     // Should return true or false based on whether or not workingDir was changed
     // Change parameters to have 1 JShell parameter, which I can use to call workingDir and fs
     // NOTE: my code currently assumes that 'input' doesn't contain "cd" as part of the string, only what comes after
@@ -12,33 +25,36 @@ public class Cd extends Command {
     // 1. Determine if input is a relative path or an absolute path
     // 2. If relative, convert to absolute path
     // 3. Find the directory in the File System. If nonexistent, output a descriptive error
+
     
-    Directory
+    Directory workingDirectory = shell.getCurrentDirectory();
+    FileSystem fs = shell.getDirectoryTree();
+    Directory root = fs.getRootDirectory();
     
-    Directory newWorkingDirectory;
-    
-    // Make sure Verifier ensures that input is nonempty
+    Directory newWorkingDirectory = null;
+    String absolutePathName;
     
     // Input is an absolute path
     if (input.charAt(0) == '/') {
-      newWorkingDirectory = fs.getDirectory(input);
+      // Even though it is an absolute path, there may still be a need to convert '..' and '.'
+      absolutePathName = getAbsolutePath(input, root);
     }
     // Input is a relative path
     else {
-      String fullPathName = getAbsolutePath(input, workingDir);
-      newWorkingDirectory = fs.getDirectory(fullPathName);
+      absolutePathName = getAbsolutePath(input, workingDirectory);    
     }
+    
+    newWorkingDirectory = fs.getDirectory(absolutePathName);
     
     if (newWorkingDirectory != null) {
-      return newWorkingDirectory;
+      shell.setCurrentDirectory(newWorkingDirectory);
     }
-    
+    else {
     // Print an appropriate error message to user and does not change working directory
-    System.out.println("Specified path not found.");
-    return workingDir; 
+    System.out.println("Specified path not found."); 
+    }
   }
   
-  // Maybe create a new Class for this method, if many commands all need to use this
   private static String getAbsolutePath(String input, Directory workingDir) {
       // Helper function for when input is a relative path name;
       // Converts relative path name to absolute path name
@@ -85,29 +101,31 @@ public class Cd extends Command {
   
   // FOR TESTING PURPOSES ONLY
   public static void main(String[] args) {
-    FileSystem fs = new FileSystem();
-    Directory root = fs.getRootDirectory();
-    
-  Directory subDir = new Directory("hello");
-  root.addSubdirectory(subDir);
-  Directory subSubdir = new Directory("world1");
-  subDir.addSubdirectory(subSubdir);
-  subSubdir = new Directory("world2");
-  subDir.addSubdirectory(subSubdir);
-  
-  if (fs.getDirectory("/hello/world2") != null) {
-    System.out.println("Successfully found");
-  }
-  else {
-    System.out.println("Directory not found");
-  }
-  
-  Cd command = new Cd();
-  Directory newWorkingDir = command.execute("hello/world1/../world2/././././..", root, fs);
-  System.out.println(newWorkingDir.getFullPathName());
-    
+//    FileSystem fs = new FileSystem();
+//    Directory root = fs.getRootDirectory();
+//    
+//  Directory subDir = new Directory("hello");
+//  root.addSubdirectory(subDir);
+//  Directory subSubdir = new Directory("world1");
+//  subDir.addSubdirectory(subSubdir);
+//  subSubdir = new Directory("world2");
+//  subDir.addSubdirectory(subSubdir);
+//  
+//  if (fs.getDirectory("/hello/world2") != null) {
+//    System.out.println("Successfully found");
+//  }
+//  else {
+//    System.out.println("Directory not found");
+//  }
+//  
+//  Cd command = new Cd();
+//  Directory newWorkingDir = command.execute("hello/world1/../world2/././././..");
+//  System.out.println(newWorkingDir.getFullPathName());
+//    
 
-    
+    Command cd = new Cd();
+    String doc = cd.getDoc();
+    System.out.println(doc);
     
     
     
