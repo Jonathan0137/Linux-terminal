47964f42085627cb01976175f799b4d4f7af37f6
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-06-22 16:58:15 +0000

    Finished Javadocs for EchoToOutput

--- Assignment2/src/driver/EchoToOutput.java
+++ Assignment2/src/driver/EchoToOutput.java
@@ -1,7 +1,12 @@
 package driver;
 
 public class EchoToOutput extends Command {
-  
+  /**
+   * Returns a String containing the documentation 
+   * for the functionalities of the 'echo' command (same documentation as EchoToFile).
+   * 
+   * @return the documentation of the 'echo' command
+   */
   @Override
   public String getDoc() {
     // Has the same documentation as EchoToFile
@@ -9,6 +14,12 @@ public class EchoToOutput extends Command {
     return etf.getDoc();
   }
   
+  /**
+   * Prints the input to the shell. 
+   * 
+   * @param shell an instance of the JShell that is interacting with the user
+   * @param input the string that will be output to the shell
+   */
   @Override 
   public void execute(JShell shell, String input) {
     System.out.println(input);
