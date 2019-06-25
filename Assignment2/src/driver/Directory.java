c3c47a71fd71bfb34dea966c9e34657853b500f6
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-06-25 19:25:33 +0000

    Finished Javadocs for Directory.java

--- Assignment2/src/driver/Directory.java
+++ Assignment2/src/driver/Directory.java
@@ -3,16 +3,22 @@ package driver;
 import java.util.ArrayList;
 
 public class Directory {
-  //TODO: Complete JavaDocs and clean up comments
-  
-  // TODO: Transfer the add files to mkdir and same thing for files (for echotofile)
-  
+  // Absolute path name of the Directory
   private String absolutePath;
+  // Name of the Directory
   private String name;
+  // The Directory's parent directory
   private Directory parentDirectory;
+  // List of subdirectories in the Directory
   private ArrayList<Directory> listOfSubdirectories;
+  // List of files in the Directory
   private ArrayList<File> listOfFiles;
   
+  /**
+   * Constructor for Directory.
+   * 
+   * @param name    the name of the directory
+   */
   public Directory(String name) {
     this.name = name;
     this.absolutePath = name + "/";
@@ -21,68 +27,13 @@ public class Directory {
     listOfFiles = new ArrayList<File>();
   }
   
-  // Removed these methods in Directory in order to not violate Single Responsibility Principle
-  // These methods should be implemented where necessary in other classes
-  
-//  // Method to add a subdirectory to the list of subdirectories
-//  public void addSubdirectory(Directory subDirectory) {
-//    // Add helper function to scan list for names
-//    if (listOfSubdirectories.contains(subDirectory) ) { // Maybe use subDirectory.getName() if that's what is differentiating them, and also check listOfFiles since files and directories cannot have same name
-//      // Not sure if Directory class is responsible for printing this error?
-//      System.out.println("Error: Directory '" + subDirectory.getName() + "' already exists.");
-//      return;
-//    }
-//    subDirectory.setParentDirectory(this);
-//    listOfSubdirectories.add(subDirectory);
-//  }
-//  
-////Method to remove a subdirectory to the list of subdirectories
-// public void removeSubdirectory(Directory subDirectory) {
-//   if (!listOfSubdirectories.contains(subDirectory) ) {
-//     // Not sure if Directory class is responsible for printing this error?
-//     System.out.println("Error: Cannot remove '" + subDirectory.getName() + "': No such directory exists."); 
-//     return;
-//   }
-//   
-//   listOfSubdirectories.remove(subDirectory);
-// }
-//  
-//  //Method to add a file to the list of files
-//  public void addFile(File file) {
-//    if (listOfFiles.contains(file) ) {
-//      System.out.println("Error: File '" + file.getName() + "' already exists.");
-//      return;
-//    }
-//    
-//    listOfFiles.add(file);
-//  }
-//  
-//  public void removeFile(File file) {
-//    if (!listOfFiles.contains(file) ) {
-//      // Not sure if Directory class is responsible for printing this error?
-//      System.out.println("Error: Cannot remove '" + file.getName() + "': No such file exists.");
-//      return;
-//    }
-//    
-//    listOfFiles.remove(file);
-//  }
-//  
-//  //TODO:
-//  // Methods to remove files and directories -- DONE
-//  // Methods to find and return files and directories in here
-//  // Methods to get and set name and parentDirectory
-//  // Methods to get listOfSubdirectories and listOfFiles
-//  
-//  
-//  public File findFile(String fileName) {
-//    for (int i=0; i<listOfFiles.size(); i++) {
-//      if (fileName.equals(listOfFiles.get(i).getName())) {
-//        return listOfFiles.get(i);
-//      }
-//    }
-//    return null;
-//  }
-  
+  /**
+   * Returns the subdirectory with the given name if it exists, otherwise returns null.
+   * 
+   * @param directoryName the name of the wanted subdirectory
+   * @return the subdirectory with the name of directoryName
+   */
+  // TODO: Move this to Command later
   public Directory findDirectory(String directoryName) {
     for (int i=0; i<listOfSubdirectories.size(); i++) {
       if (directoryName.equals(listOfSubdirectories.get(i).getName())) {
