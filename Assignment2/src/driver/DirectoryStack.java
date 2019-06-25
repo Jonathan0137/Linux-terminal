cbe464d3251697f2bf17ce6d51a98db609288bc3
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-06-25 18:59:26 +0000

    Finished Javadocs for DirectoryStack.java

--- Assignment2/src/driver/DirectoryStack.java
+++ Assignment2/src/driver/DirectoryStack.java
@@ -5,10 +5,18 @@ import java.util.LinkedList;
 public class DirectoryStack {
   private LinkedList<Directory> directoryStack;
   
+  /**
+   * Constructor to create a new DirectoryStack object.
+   */
   public DirectoryStack() {
     directoryStack = new LinkedList<Directory>();
   }
   
+  /**
+   * Returns the directory stack.
+   * 
+   * @return the directory stack.
+   */
   public LinkedList<Directory> getStack() {
     return directoryStack;
   }
