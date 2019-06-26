c9fbe2d8c6219f7419a3b8ae658f2a79400ced59
shahadi6 <shahadi6@UTORONTO.CA>
2019-06-26 02:14:19 +0000

    completed mkdir and documented mkdir and pwd

--- Assignment2/src/driver/Pwd.java
+++ Assignment2/src/driver/Pwd.java
@@ -3,62 +3,31 @@ package driver;
 
 public class Pwd extends Command{
 	
+	
+	/**
+	  * Prints the absolute path of the current working directory
+	  * 
+	  * @param shell   an instance of the JShell that is interacting with the user
+	  */
 	@Override
-	public void execute(JShell shell, String path)
+	public void execute(JShell shell, String input)
 	{
 		Directory currentDirectory = shell.getCurrentDirectory();
-		String absPath = getAbsolutePath(currentDirectory.getFullPathName(), currentDirectory);
-		System.out.println(absPath);
+		System.out.println(currentDirectory.getFullPathName());
 	}
 	
-	private static String getAbsolutePath(String input, Directory workingDir) {
-		// Helper function for when input is a relative path name;
-		// Converts relative path name to absolute path name
-		String[] pathList = input.split("/");  
-		String fullPathName = workingDir.getFullPathName();
-		  
-		for (int i=0; i<pathList.length; i++) {
-			if (pathList[i].equals(".") || pathList[i].equals("")) {
-				continue;
-			}
-			else if (pathList[i].equals("..")) {
-				fullPathName = moveToParentDirectory(fullPathName);
-			}
-			else {
-				fullPathName = fullPathName + pathList[i] + "/";
-			}
-		}
-		  
-		return fullPathName;
-	}
 	
-	private static String moveToParentDirectory(String pathName) {
-		// Moves the pathName to the path name of the parent directory
-				    
-		if (pathName.equals("/")) {
-			return pathName;
-		}
-				    
-		String newPathName = pathName;
-				    
-		if (newPathName.charAt(newPathName.length() - 1) == '/') {
-			newPathName = newPathName.substring(0, newPathName.length() - 1);
-		}
-				    
-		for (int i = newPathName.length() - 1; i>=0; i--) {
-			if (newPathName.charAt(i) == '/') {
-					break;
-			}      
-			newPathName = newPathName.substring(0, i);
-		} 
-		return newPathName;
-	}
-
-	
-
+	/**
+	  * Returns a String containing the documentation 
+	  * for the functionalities of the 'pwd' command.
+	  * 
+	  * @return the documentation of the 'pwd' command
+	  */
 	@Override
 	protected String getDoc() {
-		// TODO Auto-generated method stub
-		return null;
+		String documentation = "pwd: pwd\n"
+                			 + "\tprint the absolute path of the current working directory\n";
+		return documentation; 
 	}
+
 }
