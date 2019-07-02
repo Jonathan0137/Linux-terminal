f8df0ab684867ce8aeac83892e07f3a901efc41a
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-02 15:05:02 +0000

    Added helper method to EchoToOutput to check number of occurrences of double quotes in the input string

--- Assignment2/src/driver/EchoToOutput.java
+++ Assignment2/src/driver/EchoToOutput.java
@@ -3,7 +3,8 @@ package driver;
 public class EchoToOutput extends Command {
   /**
    * Returns a String containing the documentation 
-   * for the functionalities of the 'echo' command (same documentation as EchoToFile).
+   * for the functionalities of the 'echo' command
+   * (same documentation as EchoToFile).
    * 
    * @return the documentation of the 'echo' command
    */
@@ -13,14 +14,14 @@ public class EchoToOutput extends Command {
     EchoToFile etf = new EchoToFile();
     return etf.getDoc();
   }
-  
+
   /**
-   * Prints the input to the shell. 
+   * Prints the input to the shell.
    * 
    * @param shell an instance of the JShell that is interacting with the user
    * @param input the string that will be output to the shell
    */
-  @Override 
+  @Override
   public void execute(JShell shell, String input) {
     String[] inputSplit = input.split(" ", 2);
     if (inputSplit.length < 2) {
@@ -29,13 +30,32 @@ public class EchoToOutput extends Command {
     }
     // Ignore the 'echo' part of the input
     input = inputSplit[1];
-    
+    int count = countNumberOfOccurrences(input, '"');
+    if (count > 2) {
+      System.out.println("The input cannot contain \" (double quotes) "
+                        + "inside of the start and end double quotes.");
+      return;
+    }
     // Don't output the double quotes
     input = input.replaceAll("\"", "");
-    
-    // TODO: Reject the string and print an error message if there is a double quote in the middle
-    // of the string
-    
     System.out.println(input);
+  }
+  
+  /**
+   * Returns the number of occurrences of the target character 
+   * in the string input.
+   * 
+   * @param input  the string being checked for the target character
+   * @param target the target character being searched for in input
+   * @return       the number of occurrences of target in input
+   */
+  private static int countNumberOfOccurrences(String input, char target) {
+    int count = 0;
+    for (int i=0; i<input.length(); i++) {
+      if (input.charAt(i) == target) {
+        count++;
+      }
+    }
+    return count;
   }
 }
