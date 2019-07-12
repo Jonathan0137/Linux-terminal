608a14e08e9085bff2c2ec42e80b185f369a9270
xiegary@UTORONTO.CA <xiegary@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-12 15:13:55 +0000

    Added a new class called FileSystemNode that is a parent class for File and Directory

--- Assignment2/src/fileSystem/File.java
+++ Assignment2/src/fileSystem/File.java
@@ -6,23 +6,12 @@ package fileSystem;
  * 
  * @author Gary Xie
  */
-public class File { 
-  /**
-   * Absolute path name of the File
-   */
-  private String absolutePath;
-  /**
-   * Name of the File
-   */
-  private String name;
+public class File extends FileSystemNode { 
+
   /**
    * Text contents of the File
    */
   private String contents;
-  /**
-   * Parent directory of the File
-   */
-  private Directory parentDirectory;
   
   /**
    * Constructor for File, with no content inside.
@@ -30,10 +19,8 @@ public class File {
    * @param name    the name of the file
    */
   public File(String name) {
-    this.name = name;
+    super(name);
     this.contents = "";
-    this.absolutePath = name;
-    this.parentDirectory = null;
   }
   
   /**
@@ -43,38 +30,8 @@ public class File {
    * @param contents    the contents of the file
    */
   public File(String name, String contents) {
-    this.name = name;
+    super(name);
     this.contents = contents;
-    this.absolutePath = name;
-    this.parentDirectory = null;
-  }
-  
-  /**
-   * Returns the absolute path name of the file.
-   * 
-   * @return the file's absolute path
-   */
-  public String getFullPathName() {
-    return absolutePath;
-  }
-  
-  
-  /**
-   * Sets the name of the file.
-   * 
-   * @param name the file's new name
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  /** 
-   * Returns the file's name.
-   * 
-   * @return the name of the file
-   */
-  public String getName() {
-    return name;
   }
   
   /**
@@ -95,24 +52,5 @@ public class File {
     return contents;
   }
   
-  /**
-   * Sets the file's parent directory, and updates
-   * its absolute path name based on the parent directory.
-   * 
-   * @param parentDirectory the file's parent directory
-   */
-  public void setParentDirectory(Directory parentDirectory) {
-    this.parentDirectory = parentDirectory;
-    // Update the file's full path name using its parent directory
-    this.absolutePath = parentDirectory.getFullPathName() + name;
-  }
-  
-  /**
-   * Returns the file's parent directory.
-   * 
-   * @return the parent directory of the file.
-   */
-  public Directory getParentDirectory() {
-    return parentDirectory;
-  }
+
 }
