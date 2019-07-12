61cca64b217e51e86f4ba8891f6028b677c97ad2
Gary Xie <47544104+GaryJX@users.noreply.github.com>
2019-07-12 15:11:55 +0000

    Refactored FileSystem to follow Singleton design pattern

--- Assignment2/src/fileSystem/FileSystem.java
+++ Assignment2/src/fileSystem/FileSystem.java
@@ -7,6 +7,13 @@ package fileSystem;
  * @author Gary Xie
  */
 public class FileSystem {
+  
+  /**
+   * A static instance of the FileSystem, to 
+   * follow Singleton design pattern.
+   */
+  private static FileSystem fs=null;
+  
   /**
    * The file system's root directory, which is used 
    * to track information about the entire file system.
@@ -14,12 +21,33 @@ public class FileSystem {
   private Directory root;
   
   /**
+   * The user's current working directory.
+   */
+  private Directory workingDir;
+  
+  /**
    * Constructor for create a new FileSystem.
    */
-  public FileSystem() {
+  private FileSystem() {
     root = new Directory("");
+    workingDir = root;
   }
 
+  /**
+   * Returns the sole instance of the FileSystem
+   * (following Singleton design pattern). The first time
+   * this method is called, it will create a new 
+   * FileSystem object.
+   * 
+   * @return the instance of the FileSystem
+   */
+  public static FileSystem getFileSystem() {
+    if (fs==null) {
+      fs = new FileSystem();
+    }
+    return fs;
+  }
+  
   /**
    * Returns the root directory of the FileSystem.
    * 
@@ -28,4 +56,23 @@ public class FileSystem {
   public Directory getRootDirectory() {
     return root;
   }
+  
+  /**
+   * Updates the current working directory to newCurrent.
+   * 
+   * @param newCurrent the new working directory
+   */
+  public void setCurrentDirectory(Directory newCurrent) {
+      workingDir = newCurrent;
+  }
+  
+  /**
+   * Returns the current working directory.
+   * 
+   * @return the current working directory
+   */
+  public Directory getCurrentDirectory() {
+    return workingDir;
+  }
+  
 }
