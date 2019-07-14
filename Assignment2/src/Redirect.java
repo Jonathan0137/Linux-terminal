7c07310ca0be36db48aa2b50fa4af3ffa4b2a075
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-14 20:17:53 +0000

    created and completed checkRedirection() call

--- Assignment2/src/Redirect.java
+++ Assignment2/src/Redirect.java
@@ -1,10 +1,11 @@
 import fileSystem.Directory;
 import fileSystem.File;
+import fileSystem.FileSystem;
 
 public class Redirect {
 	private static File outputTo = null;
 	
-	private Redirect(String fileName, Directory location) {
+	/*private Redirect(String fileName, Directory location) {
 		outputTo = new File(fileName);
 		outputTo.setParentDirectory(location);
 	}
@@ -13,9 +14,25 @@ public class Redirect {
 		if (Redirect.outputTo == null) {
 			Redirect(fileName, location);
 		}
-	}
+	}*/
 	
 	public static void resetRedirection() {
 		Redirect.outputTo = null;
 	}
+	
+	public static boolean checkRedirection(String redirectCall) {
+		if (redirectCall.split(" ")[0].contentEquals(">") ||
+				redirectCall.split(" ")[0].contentEquals(">>")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static File getRedirection(FileSystem fileSystem, 
+			String potentialCall) {
+		File returnTo = null;
+		return returnTo;
+	}
+		
+	
 }
