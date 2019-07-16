f1528d6820b30dcc764e66940600131c5ef70819
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-16 18:30:52 +0000

    Formated Exit code up to standard

--- Assignment2/src/command/Exit.java
+++ Assignment2/src/command/Exit.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import driver.JShell;
 
 /**
- * Exit class is in charge of terminating JShell 
+ * Exit class is in charge of terminating JShell
+ * 
  * @author Tom Daudelin
  *
  */
 public class Exit extends Command {
-	
+
 	/**
 	 * Terminates the JShell
 	 */
@@ -18,15 +19,15 @@ public class Exit extends Command {
 		JShell shell = (JShell) param.get(0);
 		shell.exitShell();
 	}
-	
+
 	/**
 	 * Returns the documentation associated with the exit command
+	 * 
 	 * @return the documentation of the exit command
 	 */
 	public String getDoc() {
-		String doc = "exit: exit" + "\n\t" + "Terminates shell and deletes" +
-				" all memory of directory" + "\n\t\t" + "tree, user input" +
-				", directory stack and files.";
+		String doc = "exit: exit" + "\n\t" + "Terminates shell and deletes" + " all memory of "
+				+ "directory" + "\n\t\t" + "tree, user input" + ", directory stack and files.";
 		return doc;
 	}
 }
