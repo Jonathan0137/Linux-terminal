472cedb287a3459dac489313b83332a75df353e5
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-16 18:35:59 +0000

    Formated InputHistory up to standard

--- Assignment2/src/inputHistory/InputHistory.java
+++ Assignment2/src/inputHistory/InputHistory.java
@@ -1,9 +1,10 @@
 package inputHistory;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 /**
  * InputHistory class is in charge of remembering all user inputs.
+ * 
  * @author Tom Daudelin
  *
  */
@@ -12,40 +13,41 @@ public class InputHistory {
 	 * A list of all user inputs
 	 */
 	private ArrayList<String> inputList;
-	
+
 	/**
 	 * A static instance of the InputHistory
 	 */
 	private static InputHistory history = null;
-	
+
 	/**
 	 * The constructor
 	 */
 	private InputHistory() {
 		inputList = new ArrayList<String>();
 	}
-	
+
 	public static InputHistory getInputHistory() {
 		if (history == null) {
 			history = new InputHistory();
 		}
 		return history;
 	}
+
 	/**
-	 * Adds the string representation of a user's input at the end of the
-	 * list
+	 * Adds the string representation of a user's input at the end of the list
+	 * 
 	 * @param toAdd A String representation of a user's input
 	 */
 	public void addToHistory(String toAdd) {
 		this.inputList.add(toAdd);
 	}
-	
+
 	/**
-	 * Returns the list of all user inputs stored within 
-	 * the InputHistory object
+	 * Returns the list of all user inputs stored within the InputHistory object
+	 * 
 	 * @return this.inputList the ArrayList<String> instance variable
 	 */
-	public ArrayList<String> getInputList(){
+	public ArrayList<String> getInputList() {
 		return this.inputList;
 	}
 }
