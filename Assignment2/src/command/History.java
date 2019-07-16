a14c8da77491e40c79e8d2051b51dc3417be68ce
daudeli3 <55035876+TomDaudelin@users.noreply.github.com>
2019-07-16 18:24:52 +0000

    Recoded History in order to work with the current redesign

--- Assignment2/src/command/History.java
+++ Assignment2/src/command/History.java
@@ -1,7 +1,8 @@
 package command;
 
 import java.util.ArrayList;
-import driver.JShell;
+import output.Output;
+import inputHistory.InputHistory;
 
 /**
  * History class allows user access to JShells memory of all user inputs. 
@@ -9,63 +10,39 @@ import driver.JShell;
  *
  */
 public class History extends Command {
+	
 	/**
 	 * Executes the users input by printing out users total history or
 	 * optionally prints the last nth user inputs where n is an integer
 	 * @param shell An instance of the JShell class
 	 * @param input The users input
 	 */
-	public void execute(JShell shell, String input) {
+	public void execute(ArrayList<Object> params) {
+		String input = (String) params.get(0);
 		String[] splitInput = input.split(" ");
+		String output = "";
 		if (splitInput.length == 1) {
 			for (int i=0; 
-					i<shell.getInputHistory().getInputList().size(); i++) {
-				System.out.print(i+1 + ". " + shell.getInputHistory()
-				.getInputList().get(i) + "\n");
+					i<InputHistory.getInputHistory().getInputList().size(); i++) {
+				output = output.concat(i+1 + ". " + InputHistory.getInputHistory().getInputList().get(i) + "\n");
 			}
+			Output.addUserOutput(output);
 		}
 		else {
-			if (History.historyCheck(splitInput[1],
-					shell.getInputHistory().getInputList())) {
-				try {
-					for (int i=shell.getInputHistory().getInputList().size()
-							-Integer.parseInt(splitInput[1]); i<
-							shell.getInputHistory()
-							.getInputList().size(); i++) {
-						String inputAtI = shell.getInputHistory()
-								.getInputList().get(i);
-						System.out.print(i+1 + ". " + inputAtI + "\n");
-					}
+			try {
+				for (int i=InputHistory.getInputHistory().getInputList().size()
+						-Integer.parseInt(splitInput[1]); i<
+						InputHistory.getInputHistory()
+						.getInputList().size(); i++) {
+					output = output.concat(i+1 + ". " + InputHistory.getInputHistory().getInputList().get(i) + "\n");
 				}
-				catch (Exception e) {
-					this.execute(shell, "history");
-				}
+				Output.addUserOutput(output);
 			}
-		}
-	}
-	
-	/**
-	 * Verifies that a users optional parameter is valid for the history 
-	 * command
-	 * @param argument User inputs optional parameter
-	 * @param list The list of all user inputs
-	 * @return True if argument valid, False otherwise
-	 */
-	private static boolean historyCheck(String argument,
-			ArrayList<String> list) {
-		try {
-			int cap = Integer.parseInt(argument);
-			if (cap<0) {
-				System.out.println("bash: history: The second parameter; '" +
-						cap + "' is negative");
-				return false;
+			catch (Exception e) {
+				params.remove(0);
+				params.add("history");
+				this.execute(params);
 			}
-			return true;
-		}
-		catch(Exception e) {
-			System.out.println("bash: history: The second parameter; '"+ 
-					argument + "' is not an integer");
-			return false;
 		}
 	}
 	
