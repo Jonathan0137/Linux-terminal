31f9fd0b4c8d6b0d71ffcdb16f8523eb20f9223d
shahadi6 <shahadi6@UTORONTO.CA>
2019-07-21 00:45:23 +0000

    created class cp

---
+++ Assignment2/src/command/Cp.java
@@ -0,0 +1,19 @@
+package command;
+
+import java.util.ArrayList;
+
+public class Cp extends Command{
+
+	@Override
+	public void execute(ArrayList<Object> param) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected String getDoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
