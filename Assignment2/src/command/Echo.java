6251bf1e0b8b6cbdbf76f978c1d0311712b41dfa
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-21 17:23:57 +0000

    Completed Echo and added JavaDoc

--- Assignment2/src/command/Echo.java
+++ Assignment2/src/command/Echo.java
@@ -2,32 +2,41 @@ package command;
 
 import java.util.ArrayList;
 
-import driver.JShell;
+import output.Output;
 
+/**
+ * Echo class extracts a string within a user input and prints it onto shell or onto a file.
+ * 
+ * @author Tom Daudelin
+ *
+ */
 public class Echo extends Command {
 
+	/**
+	 * The execute method extracts a string the user wants to either print onto
+	 * shell or into a file.
+	 * 
+	 * @param params The list of required parameters to successfully execute
+	 *               requested command.
+	 */
 	@Override
 	public void execute(ArrayList<Object> params) {
-		
-		//make string output into output class.
-		//redirect call: try redirect on call.split[2] (exception print output onto JShell)
-		//Done
-		
+		String userInput = (String) params.get(0);
+		String textContents = userInput.split("\"")[1];
+		Output.getOutputInstance().addUserOutput(textContents);
 	}
-
+	
 	@Override
 	protected String getDoc() {
 		String doc = "echo: echo STRING [(>or>>) OUTFILE]\n\tIf no optional"
-				+ " arguments are given, print STRING onto shell.\n\t" +
-				"Otherwise;" + "\n\t\t" + "If OUTFILE file does not exist," + 
-				" create a new file in" + "\n\t\t\t" + "OUTFILE path with" + 
-				" STRING as its contents." + "\n\t\t" + "If OUTFILE file "
-				+ "and path exists, and user inputs" + "\n\t\t\t" + 
-				"argument \">\", then overwrite OUTFILE file" + "\n\t\t\t" + 
-				"with STRING." + "\n\t\t" + "If OUTFILE file and path exists," 
-				+ " and user inputs" + "\n\t\t\t" + "argument \">>\", then " + 
-				"append STRING onto" + "\n\t\t\t" + "the end of OUTFILE file.";
+				+ " arguments are given, print STRING onto shell.\n\t" + "Otherwise;" + "\n\t\t"
+				+ "If OUTFILE file does not exist," + " create a new file in" + "\n\t\t\t" 
+				+ "OUTFILE path with" + " STRING as its contents." + "\n\t\t" + "If OUTFILE file "
+				+ "and path exists, and user inputs" + "\n\t\t\t" 
+				+ "argument \">\", then overwrite OUTFILE file" + "\n\t\t\t" + "with STRING." 
+				+ "\n\t\t" + "If OUTFILE file and path exists," + " and user inputs" + "\n\t\t\t" 
+				+ "argument \">>\", then " + "append STRING onto" + "\n\t\t\t" 
+				+ "the end of OUTFILE file.";
 		return doc;
 	}
-
 }
