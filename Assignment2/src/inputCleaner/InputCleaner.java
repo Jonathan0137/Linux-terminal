6cfb6454473cb700bb506d6e7f5ed940799a1fe9
baichon1 <baichon1@UTORONTO.CA>
2019-07-21 04:15:29 +0000

    updated java doc for inputcleaner

--- Assignment2/src/inputCleaner/InputCleaner.java
+++ Assignment2/src/inputCleaner/InputCleaner.java
@@ -1,7 +1,21 @@
 package inputCleaner;
 
+/**
+ * InputCleaner is an class that change up some of the minor mistakes
+ * that user makes that would not be allowed in our code.
+ * 
+ * @author Chongmin Bai
+ */
 public final class InputCleaner 
 {
+  /**
+   * Replace all more than 1 instance of contintous space character into 
+   * one space character.
+   * 
+   * @param userInput     a string that contains what user has typed
+   * @return              an instance of String that represents the 
+   *                      cleaned version of userInput
+   */
   public static String cleanInput(String userInput)
   {
     userInput = userInput.replaceAll(" +", " ");
