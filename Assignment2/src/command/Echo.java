4072c9e781cc703082307fc03e06f31b06bc011a
daudeli3@UTORONTO.CA <daudeli3@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-24 12:53:30 +0000

    Formated Echo class code

--- Assignment2/src/command/Echo.java
+++ Assignment2/src/command/Echo.java
@@ -25,7 +25,7 @@ public class Echo extends Command {
 		int first = userInput.indexOf("\"");
 		int end = userInput.lastIndexOf("\"");
 		String textContents = "";
-		for (int i=first+1; i<end ; i++) {
+		for (int i = first + 1; i < end; i++) {
 			textContents = textContents.concat(String.valueOf(userInput.charAt(i)));
 		}
 		Output.getOutputInstance().addUserOutput(textContents);
@@ -41,10 +41,10 @@ public class Echo extends Command {
 		String doc = "echo: echo STRING [(>or>>) OUTFILE]\n\tIf no optional"
 				+ " arguments are given, print STRING onto shell.\n\t" + "Otherwise;" + "\n\t\t"
 				+ "If OUTFILE file does not exist," + " create a new file in" + "\n\t\t\t" 
-				+ "OUTFILE path with" + " STRING as its contents." + "\n\t\t" + "If OUTFILE file "
-				+ "and path exists, and user inputs" + "\n\t\t\t" 
-				+ "argument \">\", then overwrite OUTFILE file" + "\n\t\t\t" + "with STRING." 
-				+ "\n\t\t" + "If OUTFILE file and path exists," + " and user inputs" + "\n\t\t\t" 
+				+ "OUTFILE path with" + " STRING as its contents." + "\n\t\t" + "If OUTFILE file " 
+				+ "and path exists, and user inputs" + "\n\t\t\t" + "argument \">\","
+				+ " then overwrite" + " OUTFILE file" + "\n\t\t\t" + "with STRING." + "\n\t\t" 
+				+ "If OUTFILE file and" + " path exists," + " and user inputs" + "\n\t\t\t" 
 				+ "argument \">>\", then " + "append STRING onto" + "\n\t\t\t" 
 				+ "the end of OUTFILE file.";
 		return doc;
