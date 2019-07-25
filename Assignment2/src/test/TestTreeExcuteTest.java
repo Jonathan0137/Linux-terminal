21fad5ee0d6251dc007d37bf33688c950d19f081
baichon1@UTORONTO.CA <baichon1@UTORONTO.CA@4709bde8-6224-4c06-8161-6d9804f7db02>
2019-07-25 01:26:24 +0000

    finished test case for tree

---
+++ Assignment2/src/test/TestTreeExcuteTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import command.Command;
+import output.Output;
+import commandParameter.CommandParameter;
+import driver.JShell;
+import verifier.Verifier;
+
+public class TestTreeExcuteTest {
+
+  ArrayList<String> input=null;
+  JShell newJShell;
+  ArrayList<String> acutal=null;
+  @Before
+  public void setUp()
+  {
+    input=new ArrayList<String>();
+    acutal=new ArrayList<String>();
+    newJShell = new JShell();
+  }
+  @Test
+  public void testTree()
+  {
+    input.add("mkdir Folder1");
+    input.add("mkdir Folder2");
+    input.add("mkdir Folder1/Folder3");
+    input.add("mkdir Folder2/Folder4");
+    input.add("tree");
+    
+    for(String userInput : input)
+    {
+      System.out.println(userInput);
+      Command toBeExecuted = Verifier.checkUserInputCommand(userInput);
+      if (toBeExecuted != null) 
+      {
+        CommandParameter param = new CommandParameter(toBeExecuted,newJShell,userInput);
+        toBeExecuted.execute(param.getParameters());
+      }
+    }
+  
+    //System.out.print(Output.getOutputInstance().getStringOutput());
+    assertEquals("\\\n  Folder2\n    Folder4\n  Folder1\n    Folder3\n", 
+        Output.getOutputInstance().getStringOutput());
+  }
+
+}
