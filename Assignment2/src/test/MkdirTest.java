a56c2174aaa928d8cdc44765cf159cc73122015b
shahadi6 <shahadi6@UTORONTO.CA>
2019-07-28 18:21:22 +0000

    completed mkdir test

---
+++ Assignment2/src/test/MkdirTest.java
@@ -0,0 +1,68 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import command.Command;
+import commandParameter.CommandParameter;
+import driver.JShell;
+import verifier.Verifier;
+import fileSystem.*;
+
+class MkdirTest {
+
+	JShell newJShell;
+
+  	@Before
+  	public void setUp() {
+    	newJShell = new JShell();
+  	}
+	
+	@Test
+	void Mkdir1RelativeDirectory() {
+		execute("mkdir a", newJShell);
+		Directory a = (Directory) FileSystemManipulation.findFileSystemNode("/a");
+		assertEquals("/a/", a.getFullPathName());
+	}
+	
+	@Test
+	void Mkdir1AbsoluteDirectory() {
+		execute("mkdir a", newJShell);
+		execute("mkdir a/b", newJShell);
+		Directory b = (Directory) FileSystemManipulation.findFileSystemNode("/a/b");
+		assertEquals("/a/b/", b.getFullPathName());
+	}
+	
+	@Test
+	void MkdirMultipleRelativeDirectories() {
+		execute("mkdir a b c", newJShell);
+		Directory a = (Directory) FileSystemManipulation.findFileSystemNode("/a");
+		Directory b = (Directory) FileSystemManipulation.findFileSystemNode("/b");
+		Directory c = (Directory) FileSystemManipulation.findFileSystemNode("/c");
+		assertEquals("/a/", a.getFullPathName());
+		assertEquals("/b/", b.getFullPathName());
+		assertEquals("/c/", c.getFullPathName());
+	}
+	
+	@Test
+	void MkdirMultipleAbsoluteDirectory() {
+		execute("mkdir a", newJShell);
+		execute("mkdir a/b a/c", newJShell);
+		Directory b = (Directory) FileSystemManipulation.findFileSystemNode("/a/b");
+		Directory c = (Directory) FileSystemManipulation.findFileSystemNode("/a/c");
+		assertEquals("/a/b/", b.getFullPathName());
+		assertEquals("/a/c/", c.getFullPathName());
+	}
+
+	private void execute(String userInput, JShell newJShell) {
+		Command toBeExecuted = Verifier.checkUserInputCommand(userInput);
+		if (toBeExecuted != null) {
+			CommandParameter param = new CommandParameter(toBeExecuted,
+													newJShell, userInput);
+			toBeExecuted.execute(param.getParameters());
+	    }
+
+	}
+}
