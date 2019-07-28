fb85400708931e22466182bf6be23d7625c21f65
shahadi6 <shahadi6@UTORONTO.CA>
2019-07-28 16:00:09 +0000

    completed cat test

---
+++ Assignment2/src/test/CatTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import command.Command;
+import commandParameter.CommandParameter;
+import driver.JShell;
+import fileSystem.*;
+import output.Output;
+import redirection.Redirection;
+import verifier.Verifier;
+
+class CatTest {
+
+	ArrayList<String> input;
+	JShell newJShell;
+	ArrayList<Object> param;
+
+  	@Before
+  	public void setUp() {
+    	newJShell = new JShell();
+  	}
+	
+	@Test
+	void CatWith1File() {
+		execute("echo \"File1 contents\" > File1", newJShell);
+		execute("cat File1", newJShell);
+		assertEquals("File1 contents\n", 
+				Output.getOutputInstance().getStringOutput());
+		Output.getOutputInstance().resetOutput();
+	}
+	
+	@Test
+	void CatWithMultipleFiles() {
+		execute("echo \"File1 contents\" > File1", newJShell);
+		execute("echo \"File2 contents\" > File2", newJShell);
+		execute("cat File1 File2", newJShell);
+		assertEquals("File1 contents\n\n\n\nFile2 contents\n", 
+				Output.getOutputInstance().getStringOutput());
+		Output.getOutputInstance().resetOutput();
+	}
+
+	@Test
+	void CatWith1FileError() {
+		execute("cat File1", newJShell);
+		assertEquals("Error: The file 'File1' does not exist in /\n", 
+				Output.getOutputInstance().getStringOutput());
+		Output.getOutputInstance().resetOutput();
+	}
+	
+	
+	private void execute(String userInput, JShell newJShell) {
+		Command toBeExecuted = Verifier.checkUserInputCommand(userInput);
+		if (toBeExecuted != null) {
+			CommandParameter param = new CommandParameter(toBeExecuted,
+													newJShell, userInput);
+			toBeExecuted.execute(param.getParameters());
+			Redirection.redirectionSetUp(userInput);
+	    }
+
+	}
+}
